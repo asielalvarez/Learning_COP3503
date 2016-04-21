06e21721ff4bd149993f7f4d9b0cacd6061f36f2
asielalvarez <asielalvarez@hotmail.com>
2016-04-20 23:03:50 -0400

    get name method

--- Learning_Pointers/Learning_Pointers/Main.cpp
+++ Learning_Pointers/Learning_Pointers/Main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+void getName();
 
 int main() {
 	cout << "Hello World!" << endl;
+	getName();
 	cin.get();
+}
+
+void getName() {
+	string name;
+	cout << "Enter your name" << endl;
+	cin >> name;
 }
