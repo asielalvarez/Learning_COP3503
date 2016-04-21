527957acd9839a04868302232aeff8eb34f6c568
asielalvarez <asielalvarez@hotmail.com>
2016-04-20 23:16:20 -0400

    Adding lastname

--- Learning_Pointers/Learning_Pointers/Main.cpp
+++ Learning_Pointers/Learning_Pointers/Main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <string>
 using namespace std;
+void getLastName();
 
 int main() {
 	cout << "Hello World!" << endl;
+	getLastName();
 	cin.get();
+}
+
+void getLastName() {
+	string lastname;
+	cout << "Enter Last Name" << endl;
+	cin >> lastname;
 }
